6326e7a8031ffea34f93f37fc7f34a5f501e83ee
karthick7204 <karthicklimited1234@gmail.com>
2025-11-11 19:16:36 +0530

    added ternary operator

--- student-score.js
+++ student-score.js
@@ -1,64 +1,63 @@
 const student = [];
 
-function addStudents(students , marks){
-    marks = Number(marks);
-   students= students.toUpperCase();
-    student.push({name: students,marks:marks})
+function addStudents(students, marks) {
+  marks = Number(marks);
+  students = students.toUpperCase();
+  student.push({ name: students, marks: marks });
 }
 
-function getGrade(){
-    return student.map((students)=>{
-        let grade = "F"
-        if(students.marks >=90){
-          grade ="A"
-        }
-        else if(students.marks>=70 && students.marks < 90 ){
-            grade = "B"
-        }
-         else if(students.marks>=50 && students.marks < 70){
-            grade = "c"
-        }
-        else{
-            grade = "F"
-        }
-        return {...students , grade:grade}
-        
-    })
+function getGrade() {
+  return student.map((students) => {
+    let grade = "F";
+    if (students.marks >= 90) {
+      grade = "A";
+    } else if (students.marks >= 70 && students.marks < 90) {
+      grade = "B";
+    } else if (students.marks >= 50 && students.marks < 70) {
+      grade = "c";
+    } else {
+      grade = "F";
+    }
+    return { ...students, grade: grade };
+  });
 }
 
-function topScore(){
-    return student.filter((students)=>{
-        return students.marks > 90
-    })
+function topScore() {
+  return student.filter((students) => {
+    return students.marks > 90;
+  });
 }
 
-function studentFind(name){
-    return student.find((students)=>{
-        return students.name == name
-    })
+function studentFind(name) {
+  return student.find((students) => {
+    return students.name == name;
+  });
 }
 
-function hasFailes(){
-    return student.some((students)=>{
-        students.marks < 50
-    })
+function hasFailes() {
+  return student.some((students) => {
+    return students.marks < 50;
+  });
 }
 
-function display(){
-     let grades = getGrade();
-    console.log("students data")
-    console.log("all students with grades")
-    console.log("name  | score  | grade")
+function display() {
+  let grades = getGrade();
+  console.log("students data");
+  console.log("all students with grades");
+  console.log("name  | score  | grade");
 
-     grades.forEach((students)=>{
-        console.log(`${students.name} | ${students.marks} | ${students.grade}`)
-    })
+  grades.forEach((students) => {
+    console.log(`${students.name} | ${students.marks} | ${students.grade}`);
+  });
 }
 
-addStudents("ravi mohan", 99 )
-addStudents("ramesh",78)
-addStudents("suresh",44)
-display()
-console.log("top scorers:", topScore())
-console.log("FIND RAMESH:", studentFind("RAMESH"))
-console.log("failed students:", hasFailes())
+addStudents("ravi mohan", 99);
+addStudents("ramesh", 78);
+addStudents("suresh", 44);
+display();
+console.log("top scorers:", topScore());
+console.log("FIND RAMESH:", studentFind("RAMESH"));
+console.log(
+  "failed students:",
+  hasFailes() ? "yes some students failed" : "no failures"
+);
